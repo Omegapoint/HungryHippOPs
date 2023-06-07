e01665f50a2d2d2d875760a070b286b01caf8b27
William Lindblom <william.lindblom@hm.com>
2023-06-07 13:34:56 +0200

    Add spawnpoint

---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public Vector3 origin = new Vector3(0, 1f, 0); // Vector3.zero;
+    public float radius = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var randomPosition = origin; //+ Random.insideUnitSphere * radius;
+        Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
+        randomPosition.y = 0.5f;
+        randomPosition.x += 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
