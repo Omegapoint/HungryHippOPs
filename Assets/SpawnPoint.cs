3d4e300e900e5e6cce4f7ae761bd817458722f44
William Lindblom <william.lindblom@hm.com>
2023-06-07 14:13:47 +0200

    Add spawnpoint again

--- Assets/SpawnPoint.cs
+++ Assets/SpawnPoint.cs
@@ -8,11 +8,14 @@ public class SpawnPoint : MonoBehaviour
     public Vector3 origin = new Vector3(0, 1f, 0); // Vector3.zero;
     public float radius = 10;
 
+
     // Start is called before the first frame update
     void Start()
     {
         var randomPosition = origin; //+ Random.insideUnitSphere * radius;
+
         Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
+        Debug.Log("================= " + objectToSpawn.transform.position);
         randomPosition.y = 0.5f;
         randomPosition.x += 10f;
     }
