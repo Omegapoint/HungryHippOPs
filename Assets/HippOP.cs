c0fc854e95a9249d57108f7a5e6f95acf52884d9
William Lindblom <william.lindblom@hm.com>
2023-06-07 16:28:51 +0200

    Add HippOP Script

---
+++ Assets/HippOP.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HippOP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+        }
+    }
+}
