6f591f8e61367a67ddfc63b03012ac01a61465c9
William Lindblom <william.lindblom@hm.com>
2023-06-07 16:48:11 +0200

    Add rotation to hippOP

--- Assets/HippOP.cs
+++ Assets/HippOP.cs
@@ -3,9 +3,14 @@ using UnityEngine.UIElements;
 
 public class HippOP : MonoBehaviour
 {
+    private float angleOffset = 0;
+
+    private Vector3 originalForward;
+
     // Start is called before the first frame update
     void Start()
     {
+        originalForward = transform.forward;
     }
 
     // Update is called once per frame
@@ -13,10 +18,14 @@ public class HippOP : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-
+            angleOffset += 30 * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            angleOffset -= 30 * Time.deltaTime;
         }
+
+        angleOffset = Mathf.Clamp(angleOffset, -30, 30);
+        transform.forward = Quaternion.Euler(new Vector3(0, angleOffset, 0)) * originalForward;
     }
 }
